2533a0c706d01a000eeec0f9ba7039539c6417ad
dmn13081977 <shehovtzov@mail.ru>
2023-08-16 11:55:22 +0300

    :

---
+++ SpringSecurity/src/main/java/my/springsecurity/controller/MainController.java
@@ -0,0 +1,13 @@
+package my.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
