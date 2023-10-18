ff28219ac8afc9410bb9bb1ec57c9a90e3a2a4ac
dmn13081977 <shehovtzov@mail.ru>
2023-10-18 13:20:18 +0300

    :

--- SpringREST/src/main/java/my/springrest/controllers/PeopleController.java
+++ SpringREST/src/main/java/my/springrest/controllers/PeopleController.java
@@ -1,11 +1,13 @@
 package my.springrest.controllers;
 
 import jakarta.validation.Valid;
+import my.springrest.dto.PersonDTO;
 import my.springrest.models.Person;
 import my.springrest.services.PeopleService;
 import my.springrest.util.PersonErrorResponse;
 import my.springrest.util.PersonNotCreatedException;
 import my.springrest.util.PersonNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,33 +15,40 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // @Controller + @ResponseBody над каждым методом
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private  final ModelMapper modelMapper;
 
     @Autowired
-    public PeopleController(PeopleService peopleService) {
-
+    public PeopleController(PeopleService peopleService, ModelMapper modelMapper) {
         this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping()
-    public List<Person> getPeople() {
+    public List<PersonDTO> getPeople() {
 
-        return peopleService.findAll(); // Jackson конвертирует эти объекты в JSON
+        return peopleService
+                .findAll()
+                .stream()
+                .map(this::convertToPersonDTO)
+                .collect(Collectors.toList()); // Jackson конвертирует эти объекты в JSON
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable("id") int id) {
-        return peopleService.findOne(id); // Jackson конвертирует в JSON статус ответа 200
+    public PersonDTO getPerson(@PathVariable("id") int id) {
+        return convertToPersonDTO(peopleService.findOne(id)); // Jackson конвертирует в JSON статус ответа 200
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO,
                                              BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             StringBuilder errorMessage = new StringBuilder();
@@ -54,9 +63,46 @@ public class PeopleController {
             throw new PersonNotCreatedException(errorMessage.toString());
         }
 
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO));
         //отправляем HTTP ответ с пустым телом и статусом 200
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private PersonDTO convertToPersonDTO(Person person) {
+        PersonDTO personDTO = modelMapper.map(person, PersonDTO.class);
+        return personDTO;
+    }
+
+    //используем ModelMapper с помощью Spring
+    private Person convertToPerson(PersonDTO personDTO) {
+        Person person = modelMapper.map(personDTO, Person.class);
+        addDataToPerson(person);
+        return person;
+    }
+
+
+    //используем ModelMapper вручную
+//    private Person convertToPerson(PersonDTO personDTO) {
+//        ModelMapper modelMapper = new ModelMapper();
+//        Person person = modelMapper.map(personDTO, Person.class);
+//        addDataToPerson(person);
+//        return person;
+//    }
+
+      //не применимо если много полей
+//    private Person convertToPerson(PersonDTO personDTO) {
+//        Person person = new Person();
+//        person.setName(personDTO.getName());
+//        person.setAge(personDTO.getAge());
+//        person.setEmail(personDTO.getEmail());
+//        addDataToPerson(person);
+//        return person;
+//    }
+
+    private void addDataToPerson(Person person) {
+        person.setCreatedAt(LocalDateTime.now());
+        person.setUpdatedAt(LocalDateTime.now());
+        person.setCreatedWho("ADMIN");
     }
 
     @ExceptionHandler
---
+++ SpringREST/src/main/java/my/springrest/dto/PersonDTO.java
@@ -0,0 +1,45 @@
+package my.springrest.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class PersonDTO {
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should have 2-30 symbols")
+    private String name;
+
+    @Min(value = 0, message = "Age should be bigger then 0")
+    private int age;
+
+    @Email
+    @NotEmpty(message = "Email should not be empty")
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
